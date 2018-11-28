882b0176e236cf0355c4bb89eeb9a509cd8dfb9a
amjadkarym <amjadkarym@gmail.com>
2018-11-28 15:00:09 -0600

    Update Week1Day1Assignment1.js

--- assignments/Day1/Week1Day1Assignment1.js
+++ assignments/Day1/Week1Day1Assignment1.js
@@ -7,3 +7,26 @@ String.prototype.filterWords = function(arr){
 
 //run 
 // "Hello a World".filterWords(['Hello', 'World'])
+
+
+String.prototype.filterWords = function(arr){
+  return new Promise(function(resolve, reject) {
+			// prpcess hgfjhg**kjhkhj***
+			//resolve(prpcess)
+			var myStr = this.split(" ");
+			arr.map(val => myStr = myStr.map(n=>n.replace(val, '***')));
+			if(true) {
+				resolve(myStr.join(" "));
+			} else {
+				reject("error occured");
+			}
+		})
+}
+
+
+filterWords()
+	.then(data => console.log(data))
+	.catch(err => console.error(err));
+	
+
+// "Hello a World".filterWords(['Hello', 'World'])
