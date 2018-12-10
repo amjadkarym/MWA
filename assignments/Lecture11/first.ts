254cbc1740831ddf322988afc0cc10d49faff3b6
amjadkarym <amjadkarym@gmail.com>
2018-12-09 23:23:48 -0600

    Add files via upload

---
+++ assignments/Lecture11/first.ts
@@ -0,0 +1,16 @@
+class University {
+    name: string;
+    dept: string;
+
+    constructor(name: string, dept: string) {
+        this.name = name;
+        this.dept = dept;
+    }
+
+    graduation(year: number): void {
+        console.log(`Graduating ${this.dept} ${year} students`);
+    }
+}
+
+const mum: University = new University("MUM", "Computer Science");
+mum.graduation(2019);
