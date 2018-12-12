7d8b19148298bb108c28ddbc58c67706b7b195b4
amjadkarym <amjadkarym@gmail.com>
2018-12-12 00:25:44 -0600

    Update loggable.directive.ts

--- assignments/Homework12/app/loggable.directive.ts
+++ assignments/Homework12/app/loggable.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, ElementRef, Renderer2, HostListener, HostBinding } from '@angular/core';
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appLoggable]'
 })
 export class LoggableDirective {
 
-  constructor(private e: ElementRef, private r: Renderer2) { 
-    // e.nativeElement.style.fontSize = '22px'
-    r.setStyle(e.nativeElement, 'font-size', '22px')
+  constructor() { 
   }
 
-  @HostListener('mouseenter') foo() { this.e.nativeElement.style.color = 'red'; this.myBG = 'yellow' }
+
+  @HostListener("click", ["$event.target"]) clickListener(element) {
+    console.log(`${element.tagName.toUpperCase()} element has been clicked`);
+    return false;
+  }
 }
